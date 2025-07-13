08126c3078ec101f66917f91897c295da3c20448
paulettemal <136779147+paulettemal@users.noreply.github.com>
2025-07-13 15:06:30 -0500

    Update alg1_Paulette_Maldonado.rb

--- algoritmos/alg1_Paulette_Maldonado.rb
+++ algoritmos/alg1_Paulette_Maldonado.rb
@@ -1,14 +1,9 @@
 # --- Ejemplo (1): Código Correcto ---
 puts "\n[1] Ejemplo Correcto:"
-numeros_correctos = [1, 2, 3, 4, 5]
+puts "\nIteración con FOR:"
 
-def sumar_arreglo_correcto(arreglo)
-  suma = 0
-  for num in arreglo
-    suma = suma + num
-  end
-  return suma
+palabras = ["Ruby", "Parser", "PLY", "Analizador"]
+
+for palabra in palabras
+  puts "Palabra: #{palabra} (Longitud: #{palabra.length})"
 end
-
-resultado_correcto = sumar_arreglo_correcto(numeros_correctos)
-puts "La suma es: #{resultado_correcto}"
